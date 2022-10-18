b74e841c61557b47519c47d7e3fcac74c381d547
Jonas0Dias <jonaaasdiaaas@gmail.com>
2022-10-18 15:59:54 -0300

    Layout sem cartas

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let QtdCartas = 0
+// while(QtdCartas%2!==0 || QtdCartas>14 || QtdCartas<4){
+//     alert("Você deve escolher um número par entre 4 e 14, inclusive.")
+//     QtdCartas = prompt('Com quantas cartas deseja Jogar?')
+//     }
+QtdCartas =  parseInt(QtdCartas)
+
+
+    
+
+    
